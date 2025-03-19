d1ea460f380dd59e10371c283f2cedb297b3f3a9
Angelique <angelique.copere@gmail.com>
2025-03-19 17:25:05 +0100

    add heroSection

---
+++ app/components/Button.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface ButtonProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;
+  variant?: 'primary' | 'secondary';
+}
+
+const Button: React.FC<ButtonProps> = ({ href, children, className, variant = 'primary' }) => {
+  const baseClasses = 'px-4 py-2 rounded-md transition-colors duration-300 font-semibold ';
+  const variantClasses = variant === 'primary' ? 'bg-white text-black hover:bg-red hover:text-white' : 'bg-red text-white hover:bg-white hover:text-black';
+
+  return (
+    <Link href={href} className={`${baseClasses} ${variantClasses} ${className}`}>
+      {children}
+    </Link>
+  );
+};
+
+export default Button;
