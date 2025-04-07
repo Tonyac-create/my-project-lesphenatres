0c156aadda48f30e715552700c22fe69d39e67fe
Angelique <angelique.copere@gmail.com>
2025-04-07 12:23:46 +0200

    update picture team

--- app/components/Home/CardTeam.tsx
+++ app/components/Home/CardTeam.tsx
@@ -24,10 +24,10 @@ export default function CardTeam({ src, alt, name, role, description }: CardTeam
         <article className="flex flex-col justify-center items-center w-[350px] p-6">
             <Image
                 src={src}
-                width={300}
-                height={300}
+                width={250}
+                height={250}
                 alt={alt}
-                className="w-[250px] h-[250px] object-cover rounded-full mx-auto drop-shadow-[3px_7px_7px_rgba(0,0,0,0.7)] opacity-0"
+                className="w-[250px] h-[250px] object-cover rounded-full mx-auto drop-shadow-[3px_7px_7px_rgba(0,0,0,0.7)] animate-fade"
             />
             <h4 className="text-red text-3xl my-3 font-dancing font-semibold">{name}</h4>
             <p className="text-2xl text-black">{role}</p>
--- app/utils/team.ts
+++ app/utils/team.ts
@@ -1,41 +1,41 @@
 export const team = [
     {
-        src: "/Kevin.png",
+        src: "/membreCA/Kevin_president.jpg",
         alt: "Kevin Dupont",
         name: "Kevin Dupont",
         role: "Président",
         description: "Le grand manitou"
     },
     {
-        src: "/Marie.png",
-        alt: "Nicole Yvain",
-        name: "Nicole Yvain",    
-        role: "Vice-présidente",
+        src: "/membreCA/Nicole_vicepresidente.jpg",
+        alt: "Yvain Nicole",
+        name: "Yvain Nicole",    
+        role: "Vice-président",
         description: "Le bras droit du grand manitou"
     },
     {
-        src: "/Lucas.png",
+        src: "/membreCA/Julie_secretaireadjointe.jpg",
         alt: "Hugo",
         name: "Hugo Petithomme",    
         role: "Secrétaire",
         description: "Il note tout, rien ne lui échappe"
     },
     {
-        src: "/Léa.png",
+        src: "/membreCA/Hugo_secretaire.jpg",
         alt: "Julie",
         name: "Julie Salembier",    
         role: "Secrétaire adjointe",
         description: "Son backup, toujours prête"
     },
     {
-        src: "/Kevin.png",
+        src: "/membreCA/JeanClaude_tresorier.jpg",
         alt: "Jean-Claude",
         name: "Jean-Claude Dupont",    
         role: "Trésorier",
         description: "Les sous, c'est son affaire"
     },
     {
-        src: "/Pierre.png",
+        src: "/membreCA/Jordan_membreCA.jpg",
         alt: "Jordan",
         name: "Jordan Fondase",    
         role: "Membre CA",
