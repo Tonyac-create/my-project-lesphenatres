61a9834017b81d18d8db4f1046acbd8f64bfe310
Angelique <angelique.copere@gmail.com>
2025-04-20 14:56:56 +0200

    update responsive sectionteam

--- app/components/Home/SectionTeam.tsx
+++ app/components/Home/SectionTeam.tsx
@@ -3,8 +3,8 @@ import CardTeam from "./CardTeam";
 
 export default function SectionTeam() {
     return (
-        <section className="flex flex-col items-center gap-6 p-8 md:px-28 md:py-11 tracking-wide">
-            <h3 className="font-dancing text-red text-3xl md:text-5xl mb-3.5 pl-6 font-semibold flex mr-auto">{`Les membres du comité d'administration`}</h3>
+        <section className="flex flex-col items-center gap-6 px-8 md:px-28 md:pb-11 tracking-wide">
+            <h2 className="font-dancing text-red text-3xl sm:text-4xl xl:text-5xl mb-5 lg:mb-7 pl-6 font-semibold flex mr-auto">{`Les membres du comité d'administration`}</h2>
             <div className="flex flex-wrap justify-center gap-8">
                 {team.map((member, index) => (
                     <CardTeam key={index} src={member.src} alt={member.alt} name={member.name} role={member.role} description={member.description} />
