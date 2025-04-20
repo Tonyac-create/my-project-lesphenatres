da91daa5777174129da2769ac1ae534ef4a4210f
Angelique <angelique.copere@gmail.com>
2025-04-20 15:34:08 +0200

    update respnsive footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,31 +3,37 @@ import Link from "next/link"
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col md:flex-row md:justify-between px-6  bg-white text-black text-center p-4">
-            <div className="flex flex-col md:flex-row gap-4">
+        <footer className="flex flex-col gap-3 xl:flex-row md:justify-between px-6 bg-white text-black p-4">
+            <div className="flex flex-col xl:flex-row gap-4">
                 <Image
                     src="/Logo_Phenatres.webp"
                     alt="Logo Les Phénâtres"
                     width={100}
                     height={100}
-                    className="object-cover"
+                    className="object-cover xl:w-48 xl:h-48"
                 />
                 <div className="flex flex-col items-start">
-                    <h3>
+                    <h2>
                         {`Les Phénatres de l'Artois`}
-                    </h3>
+                    </h2>
                     <p>2 rue de la Douizième 62 000 Arras</p>
-                    <p>
-                        {`Email : lesphenatresdelartois62000@gmail.com`}
+                    <p className="font-semibold">
+                        {`Email : `}
                     </p>
                     <p>
-                        {`Téléphone : 06 68 37 70 32`}
+                        {`lesphenatresdelartois62000@gmail.com`}
+                    </p>
+                    <p className="font-semibold">
+                        {`Téléphone : `}
+                    </p>
+                    <p>
+                        {`06 68 37 70 32`}
                     </p>
                 </div>
             </div>
             <div className="flex flex-col gap-4">
                 <p>Suivez nos actualités</p>
-                <div className="flex gap-4">
+                <div className="flex flex-col lg:flex-row gap-4">
                     <Link href="https://www.facebook.com/lesphenatres" target="_blank" className="hover:text-blue-700 inline-flex items-center">
                         <Image
                             src="/icons/Mask_facebook.webp"
@@ -51,15 +57,15 @@ export default function Footer() {
                         TikTok
                     </Link>
                     <Link href="https://www.instagram.com/lesphenatresdelartois/?igsh=MXZwY21xeXZhOW80Ng%3D%3D#" target="_blank" className="hover:text-blue-700 inline-flex items-center">
-                    <Image
-                        src="/icons/Mask_Instagram.png"
-                        width={50}
-                        height={50}
-                        alt="Tiktok"
-                        className="object-cover rounded-full mr-2"
-                    />
-                    Instagram
-                </Link>
+                        <Image
+                            src="/icons/Mask_Instagram.webp"
+                            width={50}
+                            height={50}
+                            alt="Tiktok"
+                            className="object-cover rounded-full mr-2"
+                        />
+                        Instagram
+                    </Link>
                 </div>
             </div>
             <div className="flex flex-col gap-4">
