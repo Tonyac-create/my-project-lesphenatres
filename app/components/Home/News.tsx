cb9b7892453f94948610b66dfb06b13f5e6d718b
Angelique <angelique.copere@gmail.com>
2025-04-20 14:21:52 +0200

    update responsive news

--- app/components/Home/News.tsx
+++ app/components/Home/News.tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 export default function News() {
     return (
         <section className="flex flex-col gap-6 p-8 md:px-28 md:py-11 tracking-wide">
-            <h3 className="font-dancing text-red text-3xl md:text-5xl mb-3.5  font-semibold flex mr-auto">{`Suivez nous sur les réseaux sociaux`}</h3>
-            <p className="text-lg md:text-2xl">{`Retrouvez les moments forts de la troupe, des répétitions aux grandes premières.`}</p>
-            <p className="text-lg md:text-2xl">{`Rejoignez-nous sur les réseaux pour ne rien manquer du spectacle !`}</p>
-            <div className="flex flex-col justify-center lg:flex-row gap-20 my-5">
-                <Link href="https://www.facebook.com/lesphenatres" target="_blank" className="group inline-flex items-center gap-2 text-2xl hover:text-red">
+            <h2 className="font-dancing text-red text-3xl sm:text-4xl xl:text-5xl mb-3.5  font-semibold flex mr-auto">{`Suivez nous sur les réseaux sociaux`}</h2>
+            <div className="flex flex-col gap-2">
+                <p className="text-lg md:text-xl xl:text-2xl">{`Retrouvez les moments forts de la troupe, des répétitions aux grandes premières.`}</p>
+                <p className="text-lg md:text-xl xl:text-2xl">{`Rejoignez-nous sur les réseaux pour ne rien manquer du spectacle !`}</p>
+            </div>
+            <div className="flex flex-col justify-center lg:flex-row gap-5 xl:gap-20 my-5">
+                <Link href="https://www.facebook.com/lesphenatres" target="_blank" className="group inline-flex items-center gap-2 text-xl hover:text-red">
                     <Image
                         src="/icons/Mask_facebook.webp"
                         width={100}
@@ -18,7 +20,7 @@ export default function News() {
                     />
                     Nous suivre sur Facebook
                 </Link>
-                <Link href="https://www.tiktok.com/@lesphenatresdelartois?_t=ZN-8vLGTpttruQ&_r=1" target="_blank" className="group inline-flex items-center gap-2 text-2xl hover:text-red">
+                <Link href="https://www.tiktok.com/@lesphenatresdelartois?_t=ZN-8vLGTpttruQ&_r=1" target="_blank" className="group inline-flex items-center gap-2 text-xl hover:text-red">
                     <Image
                         src="/icons/Mask_tiktok.webp"
                         width={100}
@@ -28,7 +30,7 @@ export default function News() {
                     />
                     Nous suivre sur TikTok
                 </Link>
-                <Link href="https://www.instagram.com/lesphenatresdelartois/?igsh=MXZwY21xeXZhOW80Ng%3D%3D#" target="_blank" className="group inline-flex items-center gap-2 text-2xl hover:text-red">
+                <Link href="https://www.instagram.com/lesphenatresdelartois/?igsh=MXZwY21xeXZhOW80Ng%3D%3D#" target="_blank" className="group inline-flex items-center gap-2 text-xl hover:text-red">
                     <Image
                         src="/icons/Mask_Instagram.png"
                         width={100}
--- app/components/Home/SectionAbout.tsx
+++ app/components/Home/SectionAbout.tsx
@@ -5,7 +5,7 @@ export default function SectionAbout() {
     return (
         <section className="flex flex-col md:flex-row items-center my-9 p-8 md:px-28 md:py-11 tracking-wide">
             <article className="lg:w-[50%] text-black p-5 mb-12 rounded-md">
-                <h3 className="font-dancing text-red text-3xl sm:text-4xl mb-3.5 font-semibold">{`Comment est née l'association ?`}</h3>
+                <h2 className="font-dancing text-red text-3xl sm:text-4xl xl:text-5xl mb-3.5 font-semibold">{`Comment est née l'association ?`}</h2>
                 <div className="lg:hidden">
                     <Image
                         src="/fond_scene.webp"
@@ -16,10 +16,10 @@ export default function SectionAbout() {
                         className="w-full h-full object-cover mb-8 rounded-xl drop-shadow-[3px_5px_5px_rgba(0,0,0,0.5)]"
                     />
                 </div>
-                <p className="text-lg md:text-xl">{`Notre association a été créée en 2020, au tristement célèbre moment de la pandémie
+                <p className="text-lg md:text-xl xl:text-2xl">{`Notre association a été créée en 2020, au tristement célèbre moment de la pandémie
                  du covid 19, elle a été créée par un groupe de personnes voulant juste faire du théâtre et en partageant cette passion 
                  au plus grand nombre, tout en restant contrairement à une expérience précédente.`}</p>
-                <p className="mt-3 mb-10 text-lg md:text-xl">{`Le nom de l'association “Les phénâtres de l'Artois” est un mélange des 2 mots...`}</p>
+                <p className="mt-3 mb-10 text-lg md:text-xl xl:text-2xl">{`Le nom de l'association “Les phénâtres de l'Artois” est un mélange des 2 mots...`}</p>
                 <Button href="/philosophy" variant="secondary">{`Toute l'histoire`}</Button>
             </article>
             <div className="hidden lg:flex lg:w-[50%]">
