701001dad8dfbc04be86832e0ef412f9728d9a3d
Angelique <angelique.copere@gmail.com>
2025-05-18 14:35:06 +0200

    update

--- app/components/News/MontagePictures.tsx
+++ app/components/News/MontagePictures.tsx
@@ -6,7 +6,7 @@ export default function MontagePictures() {
     return (
         <article className="p-8 md:px-20 md:pt-11 tracking-wide">
             <h2 className="font-dancing text-red text-3xl sm:text-4xl xl:text-5xl mb-5 font-semibold">À venir</h2>
-
+            <p className="text-lg md:text-xl xl:text-2xl">Pas de dates pour le moment</p>
             <h2 className="font-dancing text-red text-3xl sm:text-4xl xl:text-5xl mt-10 mb-5 font-semibold">Passés</h2>
             <div className="flex flex-wrap gap-8">
                 <Image
